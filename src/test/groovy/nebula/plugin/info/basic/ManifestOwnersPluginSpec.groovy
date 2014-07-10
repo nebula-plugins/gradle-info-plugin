7d737f95736f06f936f450d05dbd56dff418a11f
Mike McGarr <mmcgarr@netflix.com>
2014-07-10 13:02:38 -0700

    Moving the ManifestOwnersPlugin from the gradle-contacts-plugin here

---
+++ src/test/groovy/nebula/plugin/info/basic/ManifestOwnersPluginSpec.groovy
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2014 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nebula.plugin.info.basic
+import nebula.plugin.contacts.BaseContactsPlugin
+import nebula.plugin.info.InfoBrokerPlugin
+import nebula.test.PluginProjectSpec
+
+class ManifestOwnersPluginSpec extends PluginProjectSpec {
+    @Override
+    String getPluginName() {
+        'contacts-manifest'
+    }
+
+    def 'values in broker'() {
+        def contactsPlugin = project.plugins.apply(BaseContactsPlugin)
+        project.plugins.apply(ManifestOwnersPlugin)
+        def brokerPlugin = project.plugins.apply(InfoBrokerPlugin)
+        contactsPlugin.extension.addPerson('mickey@disney.com') {
+            moniker 'Mickey Mouse'
+            role 'owner'
+        }
+        contactsPlugin.extension.addPerson('minnie@disney.com')
+        contactsPlugin.extension.addPerson('goofy@disney.com') {
+            role 'notify'
+        }
+
+        when:
+        def manifest = brokerPlugin.buildManifest()
+
+        then:
+        manifest['Module-Owner'] == 'mickey@disney.com,minnie@disney.com'
+        manifest['Module-Email'] == 'minnie@disney.com,goofy@disney.com'
+
+    }
+}
