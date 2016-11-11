9d8e45b4928382d6ea963e8ca7a1f26c28faca65
Rob Spieldenner <rspieldenner@netflix.com>
2016-11-11 11:34:35 -0800

    BUGFIX omit Resolved-Dependencies-<config> if no dependencies. fixes #34

--- src/test/groovy/nebula/plugin/info/dependency/DependenciesInfoPluginSpec.groovy
+++ src/test/groovy/nebula/plugin/info/dependency/DependenciesInfoPluginSpec.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package nebula.plugin.info.dependency
 
 import nebula.plugin.info.InfoBrokerPlugin
@@ -46,6 +61,21 @@ class DependenciesInfoPluginSpec extends PluginProjectSpec {
         'compile'       |   'com.google.guava:guava:18.0'
         'testCompile'   |   'com.google.guava:guava:18.0,junit:junit:4.11,org.hamcrest:hamcrest-core:1.3'
         'broken'        |   null
+    }
+
+    def 'omits manifest entry if no dependencies'() {
+        setup:
+        project.apply plugin: 'java'
+        def brokerPlugin = project.plugins.apply(InfoBrokerPlugin)
+        project.apply plugin: DependenciesInfoPlugin
+        project.configurations.compile.resolve()
+
+        when:
+        def manifest = brokerPlugin.buildManifest()
+
+        then:
+        noExceptionThrown()
+        !manifest.containsKey('Resolved-Dependencies-Compile')
     }
 
     @Override
