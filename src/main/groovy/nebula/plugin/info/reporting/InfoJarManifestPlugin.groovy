abd3ffb1c7ec612eed55c8dbbcd5668e6df459fb
Danny Thomas <dannyt@netflix.com>
2020-08-27 10:36:15 +1000

    Also apply property ignores to metaInf normalization

--- src/main/groovy/nebula/plugin/info/reporting/InfoJarManifestPlugin.groovy
+++ src/main/groovy/nebula/plugin/info/reporting/InfoJarManifestPlugin.groovy
@@ -18,11 +18,8 @@ package nebula.plugin.info.reporting
 
 import com.netflix.nebula.interop.GradleKt
 import nebula.plugin.info.InfoBrokerPlugin
+import nebula.plugin.info.InfoPlugin
 import nebula.plugin.info.InfoReporterPlugin
-import nebula.plugin.info.basic.BasicInfoPlugin
-import nebula.plugin.info.ci.ContinuousIntegrationInfoPlugin
-import nebula.plugin.info.java.InfoJavaPlugin
-import nebula.plugin.info.scm.ScmInfoPlugin
 import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -35,51 +32,27 @@ import org.gradle.normalization.MetaInfNormalization
  */
 class InfoJarManifestPlugin implements Plugin<Project>, InfoReporterPlugin {
 
-    private static final List<String> ATTRIBUTES_TO_IGNORE_FOR_META_INF_NORMALIZATION = [
-            'Module-Owner',
-            'Module-Email',
-            'Nebula-Version',
-            BasicInfoPlugin.BUILT_BY_PROPERTY,
-            BasicInfoPlugin.BUILT_OS_PROPERTY,
-            BasicInfoPlugin.BUILD_DATE_PROPERTY,
-            BasicInfoPlugin.GRADLE_VERSION_PROPERTY,
-            BasicInfoPlugin.BUILD_STATUS_PROPERTY,
-            InfoJavaPlugin.CREATED_PROPERTY,
-            InfoJavaPlugin.JDK_PROPERTY,
-            InfoJavaPlugin.SOURCE_PROPERTY,
-            InfoJavaPlugin.TARGET_PROPERTY,
-            ContinuousIntegrationInfoPlugin.BUILD_NUMBER_PROPERTY,
-            ContinuousIntegrationInfoPlugin.BUILD_HOST_PROPERTY,
-            ContinuousIntegrationInfoPlugin.BUILD_JOB_PROPERTY,
-            ContinuousIntegrationInfoPlugin.BUILD_ID_PROPERTY,
-            ScmInfoPlugin.MODULE_SOURCE_PROPERTY,
-            ScmInfoPlugin.MODULE_ORIGIN_PROPERTY,
-            ScmInfoPlugin.CHANGE_PROPERTY,
-            ScmInfoPlugin.FULL_CHANGE_PROPERTY,
-            ScmInfoPlugin.BRANCH_PROPERTY
-    ] as List<String>
-
     void apply(Project project) {
 
         project.plugins.withType(InfoBrokerPlugin) { InfoBrokerPlugin manifestPlugin ->
             // Searching the Gradle code base shows that Archive Tasks are the primary consumers of project.version
             project.tasks.withType(Jar).configureEach { Jar jarTask ->
                 jarTask.doFirst {
-                    Map<String,String> attrs = manifestPlugin.buildManifest()
+                    Map<String, String> attrs = manifestPlugin.buildManifest()
                     jarTask.manifest.attributes.putAll(attrs)
                 }
             }
-            if(GradleKt.versionGreaterThan(project.gradle, "6.6-rc-1")) {
+            if (GradleKt.versionGreaterThan(project.gradle, "6.6-rc-1")) {
                 configureMetaInfNormalization(project)
             }
         }
     }
 
-    private void configureMetaInfNormalization(Project project) {
+    private static void configureMetaInfNormalization(Project project) {
         project.normalization.runtimeClasspath.metaInf(new Action<MetaInfNormalization>() {
             @Override
             void execute(MetaInfNormalization metaInfNormalization) {
-                ATTRIBUTES_TO_IGNORE_FOR_META_INF_NORMALIZATION.each { attribute ->
+                InfoPlugin.NORMALIZATION_IGNORED_PROPERTY_NAMES.each { attribute ->
                     metaInfNormalization.ignoreAttribute(attribute)
                 }
             }
